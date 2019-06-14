92400cd118acfa4f2c84a06554d5925546856ab3
Patrick Naughton <patricknaughton01@gmail.com>
2019-06-14 17:26:21 -0400

    Visualization + epsilon
    Epsilon now starts high and decreases over time.
    Created method to output visualization data. Complete? except for
    the header.
    Discovered from visualization data that robot is not actually moving
    = worrying. Explains why reward is constant across episodes.

--- learn_failure/simulator.py
+++ learn_failure/simulator.py
@@ -16,7 +16,7 @@ from shapely.ops import nearest_points
 
 class Simulator(object):
 
-    def __init__(self, scene=None):
+    def __init__(self, scene=None, file=None):
         self.sim = rvo2.PyRVOSimulator(0.1, 1.0, 10, 5.0, 5.0, 0.22, 1.5)
         self.obstacles = []
         self.robot_num = None
@@ -24,6 +24,7 @@ class Simulator(object):
         self.scene = scene
         self.obs_width = 0.3
         self.build_scene(scene)
+        self.file = file
 
     def do_step(self, action_ind):
         """Run a step of the simulation.
@@ -55,6 +56,8 @@ class Simulator(object):
             )
         self.sim.setAgentVelocity(self.robot_num, vel)
         self.sim.doStep()
+        if self.file is not None:
+            self.update_visualization()
 
     def state(self):
         """Compute the state that will be fed to the DQN. This is composed
@@ -65,6 +68,99 @@ class Simulator(object):
         :return: The tensor that can be given to the DQN's value estimation
             network.
             :rtype: tensor
+
+        """
+        state = np.array(self.get_state_arr())
+        # This dimensionally works, see lines 90 - 123 in
+        # `learn_general_controller` to see why / ask Yash.
+        om = utils.build_occupancy_maps(utils.build_humans(state))[1:]
+        state = utils.transform_and_rotate(state.reshape((1, -1)))[0]
+        return torch.cat([state, om], dim=-1)
+
+    def reward(self):
+        """Calculate the reward the robot receives.
+
+        :return: The (possibly negative) reward of the robot in this time step
+            as a 1x1 tensor and whether or not the episode is over.
+            :rtype: tensor
+            :rtype: boolean
+
+        """
+        total = 0
+        # Penalties for being close to obstacles:
+        #   -0.25 for being within some threshold
+        #   -1 for colliding (radii overlap)
+        col_reward = -1
+        close_reward = -0.25
+        close_thresh = 0.2
+        robot_pos = self.sim.getAgentPosition(self.robot_num)
+        for obs in self.obstacles:
+            for vertex in obs:
+                if (self.dist(vertex, robot_pos)
+                    < self.sim.getAgentRadius(self.robot_num)):
+                    # Collision
+                    total += col_reward
+                elif (self.dist(vertex, robot_pos)
+                    < self.sim.getAgentRadius(self.robot_num) + close_thresh):
+                    # Closeness penalty
+                    total += close_reward
+        return torch.tensor([[total]], dtype=torch.float), False
+
+    def dist(self, v1, v2):
+        """Calculate the distance between v1 and v2
+
+        :param tuple v1: (x, y) of v1.
+        :param tuple v2: (x, y) of v2.
+        :return: The Euclidean distance between the two verticies.
+            :rtype: float
+
+        """
+        return ( (v1[0] - v2[0])**2 + (v1[1] - v2[1])**2 )**0.5
+
+    def update_visualization(self):
+        rpos = self.sim.getAgentPosition(self.robot_num)
+        rvel = self.sim.getAgentVelocity(self.robot_num)
+        rrad = self.sim.getAgentRadius(self.robot_num)
+        v_pref = self.sim.getAgentMaxSpeed(self.robot_num)
+        theta = math.atan2(rvel[1], rvel[0])
+        self.file.write(str(self.sim.getGlobalTime()) + " ")
+        self.file.write(str(rpos) + " ")
+        self.file.write(str(rvel) + " ")
+        self.file.write(str(rrad) + " ")
+        self.file.write(str(v_pref) + " ")
+        self.file.write(str(theta) + " ")
+        for obs in self.obstacles:
+            if len(obs) > 1:
+                # Polygonal obstacle
+                o = Polygon(obs)
+                p = Point(rpos)
+                p1, p2 = nearest_points(o, p)
+                self.file.write(str((p1.x, p1.y)) + " ")
+                self.file.write("(0, 0) ")
+                self.file.write(str(self.obs_width) + " ")
+            else:
+                # Point obstacle
+                self.file.write(str((obs[0][0], obs[0][1])) + " ")
+                self.file.write("(0, 0) ")
+                self.file.write(str(self.obs_width) + " ")
+        self.file.write("\n")
+
+    def get_state_arr(self):
+        """Calculate the state of the robot and all the obstacles in the scene.
+        Takes the form of
+        [
+            robot pos, robot vel, robot rad, robot goal, robot v_pref,
+            robot theta,
+            obs1 position, obs1 vel, obs1 rad,
+            obs2...
+            ...
+        ]
+        Note that none of these are tuples, if the value is a vector (like
+        position) its elements are just listed out.
+
+        :return: Array with the state of all the obstacles in the scene plus
+            the robot.
+            :rtype: array
 
         """
         rpos = self.sim.getAgentPosition(self.robot_num)
@@ -89,52 +185,7 @@ class Simulator(object):
             else:
                 # Point obstacle
                 state.extend([obs[0][0], obs[0][1], 0, 0, self.obs_width])
-        state = np.array(state)
-        # This dimensionally works, see lines 90 - 123 in
-        # `learn_general_controller` to see why / ask Yash.
-        om = utils.build_occupancy_maps(utils.build_humans(state))[1:]
-        state = utils.transform_and_rotate(state.reshape((1, -1)))[0]
-        return torch.cat([state, om], dim=-1)
-
-    def reward(self):
-        """Calculate the reward the robot receives.
-
-        :return: The (possibly negative) reward of the robot in this time step
-            and whether or not the episode is over.
-            :rtype: float
-            :rtype: boolean
-
-        """
-        total = 0
-        # Penalties for being close to obstacles:
-        #   -0.25 for being within some threshold
-        #   -1 for colliding (radii overlap)
-        col_reward = -1
-        close_reward = -0.25
-        close_thresh = 0.2
-        robot_pos = self.sim.getAgentPosition(self.robot_num)
-        for obs in self.obstacles:
-            for vertex in obs:
-                if (self.dist(vertex, robot_pos)
-                    < self.sim.getAgentRadius(self.robot_num)):
-                    # Collision
-                    total += col_reward
-                elif (self.dist(vertex, robot_pos)
-                    < self.sim.getAgentRadius(self.robot_num) + close_thresh):
-                    # Closeness penalty
-                    total += close_reward
-        return total, False
-
-    def dist(self, v1, v2):
-        """Calculate the distance between v1 and v2
-
-        :param tuple v1: (x, y) of v1.
-        :param tuple v2: (x, y) of v2.
-        :return: The Euclidean distance between the two verticies.
-            :rtype: float
-
-        """
-        return ( (v1[0] - v2[0])**2 + (v1[1] - v2[1])**2 )**0.5
+        return state
 
     def build_scene(self, scene):
         """Build up a scene.
@@ -214,7 +265,7 @@ class Simulator(object):
 
             # Add the robot
             self.robot_num = self.sim.addAgent(
-                (1.0, -0.15 + wall_width + wall_dist / 2.0),
+                (wall_length, -0.15 + wall_width + wall_dist / 2.0),
                 1.0, 10, 5.0, 5.0, 0.22, 1.5, (0, 0)
             )
         else:       # Build a random scene
--- learn_failure/trainer.py
+++ learn_failure/trainer.py
@@ -14,8 +14,8 @@ from utils import neg_2d_gaussian_likelihood, transform_and_rotate, build_occupa
 
 class Trainer(object):
     def __init__(self, model, config):
-        """
-        Train the trainable model of a policy
+        """Train the trainable model of a policy
+
         """
         # Policy model is used to select actions, target_model is used to
         # evaluate Q when we make our training updates. This has been shown
@@ -32,6 +32,8 @@ class Trainer(object):
         self.memory = ReplayMemory(1000000)
         self.gamma = 0.9
         self.max_timesteps = 128
+        self.epsilon = 0.9
+        self.epsilon_decay = 0.9998
 
     def run(self):
         """Run the trainer to train the policy_model such that it learns
@@ -46,28 +48,53 @@ class Trainer(object):
         print_every = self.config.get('print_every', 10)
         log_path = self.config.get('log_path', 'log')
         for episode in range(1, num_episodes + 1):
-            self.run_episode()
-            print("Ran episode {}".format(episode))
+            reward = self.run_episode(record=True, key=episode)
+            print("Ran episode {}\n\tGot reward {}".format(
+                episode, reward[0][0]
+            ))
 
-    def run_episode(self):
+    def run_episode(self, record=False, key=0):
         """Run one episode of training by creating a simulation using
         RVO2 (specifically the `Simulator` class in the `simulator` module.
         Episodes are limited to `self.max_timesteps` timesteps.
 
-        :return:
-            :rtype: None
+        :param boolean record: Whether or not to record this episode in a file
+        :param int key: The number to append to this file to make it unique
+
+        :return: 1x1 tensor containing total reward earned in this episode
+            :rtype: tensor
 
         """
-        sim = simulator.Simulator(scene="barge_in")
+        out_file = None
+        if record:
+            out_file = open(str("barge") + "_" + str(key) + ".txt", "w")
+        sim = simulator.Simulator(scene="barge_in", file=out_file)
         h_t = None
         curr_state = sim.state()
+        total_reward = torch.zeros((1, 1), dtype=torch.float)
         for i in range(self.max_timesteps):
-            action, h_t = self.policy_model.select_action(sim.state(), h_t)
+            action, h_t = self.policy_model.select_action(
+                sim.state(), h_t, epsilon=self.epsilon
+            )
+            self.epsilon *= self.epsilon_decay
             sim.do_step(action)
             reward, _ = sim.reward()
             next_state = sim.state()
-            self.memory.push(curr_state, h_t, action, next_state, reward)
+            if h_t is not None:
+                self.memory.push(
+                    curr_state, h_t[0], h_t[1], action, next_state, reward
+                )
+            else:
+                self.memory.push(
+                    curr_state, torch.zeros((1, 256)), torch.zeros((1, 256)),
+                    action, next_state, reward
+                )
             curr_state = next_state
+            total_reward += reward
+            self.optimize_model()
+        if record:
+            out_file.close()
+        return total_reward
 
     def optimize_model(self):
         """Does one step of the optimization of the policy_model by sampling
@@ -82,36 +109,29 @@ class Trainer(object):
         if len(self.memory) < self.batch_size:
             return
         transitions = self.memory.sample(self.batch_size)
-        # Transpose the batch i.e. turn a batch of transitions into a
-        # transition of batch arrays
-        batch = Transition(*zip(*transitions))
-        state_batch = torch.cat(batch.state)
-        h_t_batch = torch.cat(batch.h_t)
-        action_batch = torch.cat(batch.action)
-        next_state_batch = torch.cat(batch.next_state)
-        reward_batch = torch.cat(batch.reward)
 
-        # Compute the Q values we expected
-        state_action_values = self.policy_model(state_batch, h_t_batch).gather(1,
-            action_batch)
-        # Compute expected Q values based on Bellman equation
-        next_state_values = self.target_model(next_state_batch,
-            h_t_batch).max(1)[0].detach()
-        expected_state_action_vals = ((next_state_values * self.gamma)
-            + reward_batch)
-        # Compute loss
-        loss = F.smooth_l1_loss(state_action_values,
-            expected_state_action_vals.unsqueeze(1))
-        # Optimize
+        total_loss = Variable(torch.zeros(1), requires_grad=True).clone()
+        for transition in transitions:
+            state_action_value = self.policy_model(
+                transition.state, (transition.h_t, transition.c_t)
+            )[0].gather(1, transition.action)  #?
+            next_state_value = self.target_model(
+                transition.next_state, (transition.h_t, transition.c_t)
+            )[0].max(1)[0].detach()
+            expected_state_action_val = ((next_state_value * self.gamma)
+                + transition.reward)
+            total_loss += F.smooth_l1_loss(
+                state_action_value, expected_state_action_val
+            ) / len(transitions)
         self.optimizer.zero_grad()
-        loss.backward()
+        total_loss.backward()
         self.optimizer.step()
 
 
 # Store transitions from state to next_state via action and what reward
 # we received as well as the hidden state at that point
 Transition = namedtuple('Transition',
-    ('state', 'h_t', 'action', 'next_state', 'reward'))
+    ('state', 'h_t', 'c_t', 'action', 'next_state', 'reward'))
 
 
 class ReplayMemory(object):
