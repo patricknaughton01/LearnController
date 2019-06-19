ea2ac068ed82c9bfc2f00170dc36cde238174551
Patrick Naughton <patricknaughton01@gmail.com>
2019-06-19 17:53:58 -0400

    Tested the trained model for barging in.
    Displays odd behavior:
        Goes very far from the original corridor
        Goes very close to obstacles (specifically the lower wall)
    
        Perhaps because of just not enough training? Need a more
        robust/stricter convergence condition?

---
+++ learn_failure/test.py
@@ -0,0 +1,41 @@
+import torch
+import configparser
+
+from rl_model import Controller
+from utils import *
+from simulator import Simulator
+
+
+def main():
+    model_path = "model.tar"
+    scene = "barge_in"
+    timesteps = 100
+    out_path = "barge_0.txt"
+    out_file = open(out_path, "w")
+    epsilon = 0.05
+    args = parse_args()
+    model_config = configparser.RawConfigParser()
+    model_config.read(args.model_config)
+    model = Controller(model_config,
+                       model_type=args.model_type)  # model_type = crossing
+    model.load_state_dict(torch.load(model_path)["state_dict"])
+    model.eval()
+    sim = Simulator(scene=scene, file=out_file)
+    h_t = None
+    total_reward = torch.zeros((1, 1), dtype=torch.float)
+    for t in range(timesteps):
+        action, h_t = model.select_action(
+            sim.state(), h_t, epsilon=epsilon
+        )
+        sim.do_step(action)
+        reward, _ = sim.reward()
+        # Penalize moving
+        if action.item() != 0:
+            reward -= 0.01
+        total_reward += reward
+    print("Reward: ", total_reward.item())
+    out_file.close()
+
+
+if __name__ == "__main__":
+    main()
