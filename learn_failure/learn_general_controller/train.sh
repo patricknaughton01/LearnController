0a5c9f50c60896e31103ebfeeff4ef461706d26b
Patrick Naughton <patricknaughton01@gmail.com>
2019-11-01 12:19:25 -0500

    filling out draft + another success model

--- learn_failure/learn_general_controller/train.sh
+++ learn_failure/learn_general_controller/train.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 python train.py \
---num_epochs 1000 \
+--num_epochs 300 \
 --print_every 10 \
 --M 1 \
 --train_data_name simulate_barge_in_0.1_patrick2 \
 --test_data_name simulate_barge_in_0.1_patrick2 \
---dropout 0.4
+--dropout 0.3
