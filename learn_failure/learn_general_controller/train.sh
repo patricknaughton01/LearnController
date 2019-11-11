91eca5f1d6ee8dda5f60815d528fa899a0c7a5a2
Patrick Naughton <patricknaughton01@gmail.com>
2019-11-11 08:32:19 -0600

    Tested decreasing lstm to 16 units - failure
    Will try completely eliminating the lstm.

--- learn_failure/learn_general_controller/train.sh
+++ learn_failure/learn_general_controller/train.sh
@@ -7,5 +7,4 @@ python train.py \
 --train_data_name simulate_barge_in_0.25_patrick_move_goal \
 --test_data_name simulate_barge_in_0.25_patrick_move_goal \
 --dropout 0.4 \
---seed 257 \
---model_path /home/patricknaughton01/Downloads/LearnControllers/learn_failure/learn_general_controller/log/simulate_barge_in_0.25_patrick_move_goal/seed_25123456_bootstrap_False_M_1/model_m_0.tar
+--seed 2516
