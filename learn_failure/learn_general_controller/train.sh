eefff9c6666779a15b4b18c431cf01f4d52bd70f
Patrick Naughton <patricknaughton01@gmail.com>
2019-12-19 21:46:12 -0600

    WIP, use less data

--- learn_failure/learn_general_controller/train.sh
+++ learn_failure/learn_general_controller/train.sh
@@ -4,8 +4,9 @@ python train.py \
 --num_epochs 200 \
 --print_every 1 \
 --M 1 \
---train_data_name simulate_barge_in_heading \
---test_data_name simulate_barge_in_heading \
+--train_data_name sim_barge_in_heading_less_data \
+--test_data_name sim_barge_in_heading_less_data \
 --dropout 0.5 \
---seed 4567 \
---batch_size 32
+--seed 5678 \
+--batch_size 32 \
+--model_path log/sim_barge_in_heading_less_data/seed_4567_bootstrap_False_M_1/model_m_0.tar
--- learn_failure/test.sh
+++ learn_failure/test.sh
@@ -2,7 +2,7 @@
 
 python test.py \
 --max_timesteps 0 --num_episodes 10 --scene dynamic_barge_in_success \
---success_max_ts 200 \
---success_path learn_general_controller/log/simulate_barge_in_heading/seed_4567_bootstrap_False_M_1/model_m_0.tar \
+--success_max_ts 50 \
+--success_path learn_general_controller/log/sim_barge_in_heading_less_data/seed_5678_bootstrap_False_M_1/model_m_0.tar \
 --model_path tests/dynamic_barge/dynamic_barge_model_38/model_initsuccess.tar \
 --epsilon=0
