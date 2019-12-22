fc63be38e7678d736fbb1ffdfccb63fda661ca4f
Patrick Naughton <patricknaughton01@gmail.com>
2019-12-21 20:49:04 -0600

    training on just 50 episodes

--- learn_failure/learn_general_controller/train.sh
+++ learn_failure/learn_general_controller/train.sh
@@ -7,5 +7,6 @@ python train.py \
 --train_data_name sim_barge_in_50 \
 --test_data_name sim_barge_in_50 \
 --dropout 0.5 \
---seed 1234 \
---batch_size 32
+--seed 3456 \
+--batch_size 32 \
+--model_path log/sim_barge_in_50/seed_2345_bootstrap_False_M_1/model_m_0.tar
