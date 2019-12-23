07a8ad61aefa822be994f715f08c4b43965efee0
Patrick Naughton <patricknaughton01@gmail.com>
2019-12-22 21:46:20 -0600

    tested training on one run

--- learn_failure/learn_general_controller/train.sh
+++ learn_failure/learn_general_controller/train.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 python train.py \
---num_epochs 200 \
+--num_epochs 100000 \
 --print_every 1 \
 --M 1 \
---train_data_name sim_barge_in_50 \
---test_data_name sim_barge_in_50 \
+--train_data_name sim_barge_in_1 \
+--test_data_name sim_barge_in_1 \
 --dropout 0.5 \
---seed 3456 \
---batch_size 32 \
---model_path log/sim_barge_in_50/seed_2345_bootstrap_False_M_1/model_m_0.tar
+--seed 2345 \
+--batch_size 1 \
+--model_path log/sim_barge_in_1/seed_234_bootstrap_False_M_1/model_m_0.tar
