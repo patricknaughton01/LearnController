67534b680cba96aed85d5ceb1da4ffaa41e0f2ad
Patrick Naughton <patricknaughton01@gmail.com>
2019-12-26 16:51:34 -0600

    Retested forward controller - still works well

--- learn_failure/test.sh
+++ learn_failure/test.sh
@@ -3,7 +3,6 @@
 python test.py \
 --max_timesteps 0 --num_episodes 10 --scene dynamic_barge_in_success \
 --success_max_ts 10 \
---success_path learn_general_controller/log/sim_barge_in_short/seed_4567_bootstrap_False_M_1_reverse_True/model_m_0.tar \
+--success_path learn_general_controller/log/sim_barge_in_short/seed_34_bootstrap_False_M_1/model_m_0.tar \
 --model_path tests/dynamic_barge/dynamic_barge_model_38/model_initsuccess.tar \
 --epsilon=0 \
---reverse
