30c3c2c29e67fc04db8989c104986d2299b6b79c
Patrick Naughton <patricknaughton01@gmail.com>
2020-01-25 21:04:29 -0600

    First tests of full pipeline - success so far
    Robot seems to perform reasonable actions when it transitions to the
    failure controller.

--- learn_failure/train.sh
+++ learn_failure/train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python train.py \
---max_timesteps 10 --num_episodes 1000 --name short_initsuccess \
+--max_timesteps 10 --num_episodes 10000 --name short_initsuccess_long \
 --success_max_ts 10 \
 --success_path learn_general_controller/log/sim_barge_in_short_safe_2/seed_3_bootstrap_False_M_1_reverse_False/model_m_0.tar \
 --reverse_path learn_general_controller/log/sim_barge_in_short_safe_2_train3_single/seed_2_bootstrap_False_M_1_reverse_True/model_m_0.tar
